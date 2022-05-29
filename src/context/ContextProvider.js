07ffcfdc57c689303522da3e26ae3fa2d452974c
shreyk17 <shreyk.11@gmail.com>
2022-05-29 12:41:20 +0530

    dark mode changes

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -17,6 +17,30 @@ export const ContextProvider = ({ children }) => {
 
     const [screenSize, setScreenSize] = useState(undefined)
 
+    const [currentColor, setCurrentColor] = useState('#03C9D7')
+
+    const [currentMode, setCurrentMode] = useState("Light")
+
+    const [themeSettings, setThemeSettings] = useState(false)
+
+    const setMode = (e) => {
+        console.log(e.target.value)
+        setCurrentMode(e.target.value)
+
+        localStorage.setItem('themeMode', e.target.value)
+
+        setThemeSettings(false)
+    }
+
+    const setColor = (color) => {
+        console.log(color)
+        setCurrentColor(color)
+
+        localStorage.setItem('colorMode', color)
+
+        setThemeSettings(false)
+    }
+
     const handleClick = (clicked) => {
         setIsClicked({
             ...initialState,
@@ -33,8 +57,17 @@ export const ContextProvider = ({ children }) => {
                 setIsClicked,
                 handleClick,
                 screenSize,
-                setScreenSize
+                setScreenSize,
+                currentColor,
+                currentMode,
+                setCurrentColor,
+                setCurrentMode,
+                themeSettings,
+                setThemeSettings,
+                setMode,
+                setColor
             }}
+
         >
             {children}
         </StateContext.Provider>
